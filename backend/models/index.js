b59b577e2c72b8366c92c77c175ec070ed18db55
Nandini Midde <98312720+nandini252005@users.noreply.github.com>
2026-02-07 18:43:05 +0530

    Add Mongoose models for users, items, and claims

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,1 +1,9 @@
-module.exports = {};
+const User = require('./user.model');
+const Item = require('./item.model');
+const Claim = require('./claim.model');
+
+module.exports = {
+  User,
+  Item,
+  Claim,
+};
